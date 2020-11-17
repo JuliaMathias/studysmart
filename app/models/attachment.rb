5763cf2781a0365f27ec1832eb6bdca1bdf195f2
Julia Mathias <juliamss@gmail.com>
2020-11-17 20:22:30 -0300

    Modelos validados

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,12 @@
 class Attachment < ApplicationRecord
   belongs_to :session
+
+  validates :type, presence: true
+  validates :name, presence: true
+
+   enum type: {
+    video: 0,
+    reading: 1,
+    quiz: 2
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_study_groups
+
+  has_many :study_groups, through: :user_study_groups
+  has_many :sessions, through: :study_groups
 end
