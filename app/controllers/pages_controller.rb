ba86d9e7ff16c3abb478f47d50a6474e217b343c
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-17 21:35:44 -0300

    Customize Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # if params[:query].present?
+    #   @study_groups = policy_scope(StudyGroup).search(params[:query])
+    # else
+    #   @study_groups = []
+    # end
   end
 end
