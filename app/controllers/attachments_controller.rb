0560ae3ada75816fec3a201d77831b881e4cc7a3
Julia Mathias <juliamss@gmail.com>
2020-11-21 12:22:44 -0300

    Created attachment controller, edited session controller, created create method, altered routes

---
+++ app/controllers/attachments_controller.rb
@@ -0,0 +1,46 @@
+class AttachmentsController < ApplicationController
+  # before_action :set_session, only: [:show]
+ skip_before_action :authenticate_user!, only: [:index, :show]
+
+  # def index
+  #   @study_groups = policy_scope(StudyGroup).order(created_at: :desc)
+  # end
+
+  def show
+     authorize(@attachment)
+   end
+
+  def new
+    @session = Session.find(params[:session_id])
+    @attachment = Attachment.new
+    authorize(@attachment)
+  end
+
+  def create
+    @attachment = Attachment.new(attachment_params)
+    # attach
+    authorize(@attachment)
+    @attachment.session = Session.find(params[:session_id])
+    if @attachment.save
+      redirect_to session_path(@session)
+    else
+      redirect_to root_path
+    end
+  end
+
+
+
+
+  private
+
+  # def set_session
+  #   @session = Session.find(params[:id])
+  #   authorize(@session)
+  # end
+
+  # 1. checar se o usúario incluiu attachments ou não
+
+  def attachment_params
+    params.require(:attachment).permit(:type, :name, :url)
+  end
+end
