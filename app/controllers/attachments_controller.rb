964c3277ad2d80124d0e8ad7a9d9d60a82504642
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-26 14:48:00 -0300

    Simple refact.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,15 +1,15 @@
 class AttachmentsController < ApplicationController
   # before_action :set_session, only: [:show]
- skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
- YOUTUBE_BASE_URL = "https://www.youtube.com/embed/"
+  YOUTUBE_BASE_URL = "https://www.youtube.com/embed/"
   # def index
   #   @study_groups = policy_scope(StudyGroup).order(created_at: :desc)
   # end
 
   def show
-     authorize(@attachment)
-   end
+    authorize(@attachment)
+  end
 
   def create
     if params[:attachment][:attachment_type] == "video"
@@ -18,15 +18,8 @@ class AttachmentsController < ApplicationController
     @attachment = Attachment.new(attachment_params)
     authorize(@attachment)
     @attachment.session = Session.find(params[:session_id])
-    if @attachment.save
-      redirect_to session_path(@attachment.session)
-    else
-      redirect_to root_path
-    end
+    @attachment.save ? (redirect_to session_path(@attachment.session)) : (redirect_to root_path)
   end
-
-
-
 
   private
 
