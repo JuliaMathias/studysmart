2c8d495620eb64d0e11b25bbfdcab9458333a853
Julia Mathias <juliamss@gmail.com>
2020-11-30 20:45:54 -0300

    Made dropdown work and aligned it right

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,17 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo])
+  end
+
   private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
-  end
 end
