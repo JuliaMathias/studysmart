30aab7f2ae09e32bc615da76af52d187f25c3c9e
Insight-Creator <luizbuchhalter@Outlook.com>
2020-12-01 21:24:16 -0300

    Fix avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
   layout "withlogo", :except => [:home]
 
@@ -14,8 +15,6 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo])
@@ -26,5 +25,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-
 end
