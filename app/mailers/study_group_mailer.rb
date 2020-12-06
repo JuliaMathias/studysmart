c823c8799e11cc205209df3031e961e729acb143
Insight-Creator <luizbuchhalter@Outlook.com>
2020-12-05 23:14:27 -0300

    Mailers for study_group

---
+++ app/mailers/study_group_mailer.rb
@@ -0,0 +1,16 @@
+class StudyGroupMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.study_group_mailer.invite.subject
+  #
+  def invite
+    @study_group = params[:study_group]
+    @email = params[:email]
+    mail(
+      to:       @email,
+      subject:  "Study Group created!"
+    )
+  end
+end
