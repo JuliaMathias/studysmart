48b7f906a7ef0415ec12a187dd02882eaab06037
Insight-Creator <luizbuchhalter@Outlook.com>
2020-12-07 19:50:07 -0300

    Updated

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,7 @@
 class Session < ApplicationRecord
   belongs_to :study_group
 
-  has_many :attachments
+  has_many :attachments, dependent: :destroy
   has_many_attached :photos
   has_many_attached :pdf
 
